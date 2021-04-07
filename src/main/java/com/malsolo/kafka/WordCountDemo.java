1b9939a7ceab6659a1e3dc78cba7dd40cdd1f0b6
Javier Beneito Barquero <malsolo.com@outlook.com>
2021-04-07 07:00:17 +0200

    kafka-streams: word count demo, minor improvements.

--- src/main/java/com/malsolo/kafka/WordCountDemo.java
+++ src/main/java/com/malsolo/kafka/WordCountDemo.java
@@ -107,6 +107,11 @@ public class WordCountDemo {
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        // Records should be flushed every 10 seconds. This is less than the default
+        // in order to keep this example interactive.
+        props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10 * 1000);
+        // For illustrative purposes we disable record caches.
+        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
 
         return props;
     }
